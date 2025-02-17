22d3bd8499f6effdcdc86cb2c5b63441deaf2c5c
rafaell22 <rafael.a.b.lanfredi@gmail.com>
2025-02-16 22:25:52 -0600

    Updated forge config for win32 builds.

--- forge.config.js
+++ forge.config.js
@@ -4,12 +4,23 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './public/assets/button_app_icon'
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
       config: {},
     },
   ],
