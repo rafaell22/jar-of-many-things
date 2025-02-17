d0c549606b6fbebc157c3bcd8e9c9736e47feacc
rafaell22 <rafael.a.b.lanfredi@gmail.com>
2025-02-16 21:58:55 -0600

    Added electron. Removed axios.

---
+++ main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron/main';
+import initLocalServer from './server.js';
+
+initLocalServer();
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    title: "Jar of Many Things",
+    autoHideMenuBar: true,
+  });
+
+  //win.webContents.openDevTools();
+
+  win.loadFile("./public/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
